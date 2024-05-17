1e1a108a0fb891f7dd89c201f1e0c7f539803cce
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2024-05-17 22:04:40 +0300

    CourseDetails (function)

---
+++ app/src/main/java/app/romail/mpp_auth/CourseDetailsActivity.java
@@ -0,0 +1,62 @@
+package app.romail.mpp_auth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+
+public class CourseDetailsActivity extends AppCompatActivity implements CourseDetailsAdapter.ItemClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_details);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String courseId = String.valueOf(intent.getIntExtra("courseId",0));
+
+        JSONArray attendances = HttpRequest.GetRequestArray(this, "courses/attendance/course/"+courseId);
+        if (attendances.length() == 0) {
+            // No courses found
+            return;
+        }
+        JSONArray students = new JSONArray();
+        for (int i = 0; i < attendances.length(); i++) {
+            try {
+                JSONObject student = HttpRequest.GetRequest(this, "/student/getByEnrollmentId/"+attendances.getJSONObject(i).getString("subjectEnrollment_id"));
+                student.put("attendanceDate", attendances.getJSONObject(i).getString("date"));
+                students.put(student);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        // Display the attendances
+        RecyclerView recyclerView = findViewById(R.id.courseDetailsList);
+        CourseDetailsAdapter customAdapter = new CourseDetailsAdapter(students);
+        recyclerView.setAdapter(customAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(Objects.requireNonNull(AppCompatResources.getDrawable(this, R.drawable.linear_divider)));
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        customAdapter.setClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + position, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/app/romail/mpp_auth/CourseDetailsAdapter.java
@@ -0,0 +1,122 @@
+package app.romail.mpp_auth;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CourseDetailsAdapter extends RecyclerView.Adapter<CourseDetailsAdapter.ViewHolder> {
+
+    private final JSONArray localDataSet;
+    private ItemClickListener listener;
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.listener = itemClickListener;
+    }
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener {
+
+//        private final CardView cardView;
+
+//        private final TextView courseName;
+//        private final TextView courseDescription;
+//        private final TextView courseCode;
+//
+//        private final TableRow tableRow;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+            Log.d("ViewHolder", "ViewHolder: ");
+            Log.d("ViewHolder", "ViewHolder: "+ view);
+
+//            courseName = view.findViewById(R.id.courseName);
+//            courseDescription = view.findViewById(R.id.courseDescription);
+//            courseCode = view.findViewById(R.id.courseCode);
+//            tableRow = view.findViewById(R.id.subjectsTable);
+//            tableRow.setOnClickListener(this);
+            //textView = (TextView) view.findViewById(R.id.textView);
+//            cardView = (CardView) view.findViewById(R.id.cardView);
+//            cardView.setOnClickListener(this);
+        }
+
+//        public CardView getCardView() {
+//            return cardView;
+//        }
+
+
+        @Override
+        public void onClick(View v) {
+            if (listener != null) {
+                try {
+                    JSONObject subject = localDataSet.getJSONObject(getAdapterPosition());
+                    listener.onItemClick(v, subject.getInt("id"));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Initialize the dataset of the Adapter
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView
+     */
+    public CourseDetailsAdapter(JSONArray dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.attendance_row_item, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        Log.d("SubjectListAdapter", "onBindViewHolder called for position "+position);
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        try {
+            JSONObject attendance = localDataSet.getJSONObject(position);
+//            viewHolder.getCourseName().setText(course.getString("name"));
+//            viewHolder.getCourseDescription().setText(course.getString("description"));
+//            viewHolder.getCourseCode().setText(course.getString("code"));
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.length();
+    }
+
+
+}
